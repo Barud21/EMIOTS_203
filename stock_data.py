1be3976f75a000b99143bd85a3effe37f7f419cf
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2020-09-16 20:25:31 +0200

    Initial commit

---
+++ stock_data.py
@@ -0,0 +1,68 @@
+import csv
+import pandas as pd
+import yfinance as yf
+from yahoofinancials import YahooFinancials
+import datetime
+import os.path
+from os import path
+
+
+class StockData:
+    def __init__(self, *args, **kwargs):
+        self.startDate = '2020-07-29'
+        self.endDate = '2020-09-05'
+        self.date = datetime.date.today().strftime('%Y-%m-%d')
+        self.date_60 = (datetime.date.today() + datetime.timedelta(-59)).strftime('%Y-%m-%d')
+        self.interval = '5m'
+        self.ticker = "TSLA"
+        self.data = pd.DataFrame()
+        self.historic = pd.DataFrame()
+    
+    def updatingStockData(self):
+        if path.exists('concat.csv'):
+            dataFile = pd.read_csv('concat.csv')
+            bottom_row = dataFile.tail(1)
+            last_date = bottom_row.iloc[0]['Datetime']
+            last_date = last_date[0:10]
+            last_date = datetime.datetime.strptime(last_date, '%Y-%m-%d')
+            print(last_date)
+            print(datetime.date.today())
+            print((datetime.date.today()-last_date).days)
+            # updatedData = yf.download(  self.ticker,
+            #                             start=last_date,
+            #                             end=self.date,
+            #                             interval=self.interval,
+            #                             progress=False
+            # )
+        
+    
+    def savingStockData(self):
+        if path.exists('concat.csv'):
+            dataFile = pd.read_csv('concat.csv')
+            bottom_row = dataFile.tail(1)
+            last_date = bottom_row.iloc[0]['Datetime']
+            location = self.data.index.get_loc(last_date)
+            print(location)
+            
+            newData = self.data.truncate(before=last_date)
+            newData = newData.drop(newData.index[[0]])
+            print(newData)
+            concatData = pd.concat([self.data, newData])
+            concatData.to_csv('concat.csv')
+            
+        else:
+            self.data.to_csv('concat.csv')
+    
+    def gettingHistoricalData(self):
+        self.historic = yf.download(self.ticker,
+                                    start='2017-01-01',
+                                    end='2020-07-14',
+                                    interval='1d',
+                                    progress=False
+                                    )
+        self.historic.to_csv('historic.csv')
+
+
+if __name__ == '__main__':
+    stock = StockData()
+    stock.updatingStockData()
