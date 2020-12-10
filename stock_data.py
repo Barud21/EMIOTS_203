a3c179a094aafa1b175409983d92f6bf8c07fbb7
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2020-12-10 20:37:39 +0100

    Plot creation and export to .png were added

--- stock_data.py
+++ stock_data.py
@@ -4,6 +4,7 @@ from os import path
 import pandas as pd
 import yfinance as yf
 import matplotlib.pyplot as plt
+import mpld3
 
 
 class StockData:
@@ -89,9 +90,6 @@ class StockData:
 
             concatData = pd.concat([historicData, last60Days])
             concatData.to_csv('data/stock_data.csv')
-            # print(historicData)
-            # print(last60Days)
-            # print(concatData)
 
     def extractingTweetsDates(self):
         self.tweets = pd.read_csv('data/elonmusk_Tweets.csv', index_col=0)
@@ -107,21 +105,67 @@ class StockData:
         print('***************')
 
         for x in range(-3, -1):   # self.tweets.shape[0]
-            # print(self.tweets.iloc[x, 0])
-            idx = self.data.index.get_loc(key=self.tweetsDates.iloc[x, 0], method='pad')
-            firstHour = self.data.iloc[idx:idx+13]
-            firstHourHighestPeak = firstHour["High"].max()
-            firstHourLowestPeak = firstHour["Low"].min()
+            print(self.tweets.iloc[x, 0])
+            idx = self.data.index.get_loc(key=self.tweetsDates.iloc[x, 0], method='pad')    # finding last stock
+            # price before tweet
+
+            firstHour = self.data.iloc[idx:idx+13]  # collecting data from first hour after tweet
+            firstHourHighestPeak = firstHour["High"].max()  # finding max stock price
+            firstHourLowestPeak = firstHour["Low"].min()    # finding min stock price
             openingValue = firstHour["Open"][0]
 
-            swingInPlus = (firstHourHighestPeak - openingValue)/openingValue*100
-            swingInMinus = (firstHourLowestPeak - openingValue)/openingValue*100
+            swingInPlus = (firstHourHighestPeak - openingValue)/openingValue*100    # calculating swing in plus
+            swingInMinus = (firstHourLowestPeak - openingValue)/openingValue*100    # calculating swing in minus
 
-            sixHoursSpan = self.data.iloc[idx-36: idx+37]
-            sixHoursSpan.index = sixHoursSpan.index.format(formatter=lambda x: x.strftime('%H:%M'))
+            threeHoursBefore = self.data.iloc[idx-36: idx+1]
+            threeHoursBefore.index = threeHoursBefore.index.format(formatter=lambda x: x.strftime('%H:%M'))
 
-            sixHoursSpan["Open"].plot(legend=True, grid=True)
-            sixHoursSpan["Volume"].plot(secondary_y=True, legend=True, grid=True)
+            threeHoursAfter = self.data.iloc[idx+1: idx+37]
+            threeHoursAfter.index = threeHoursAfter.index.format(formatter=lambda x: x.strftime('%H:%M'))
+
+            tweetTimeIdx = datetime.datetime.strptime(self.tweets.iloc[x, 0], '%Y-%m-%d %H:%M:%S')  #TODO: create tmp for this and 2nd line of for
+            tweetTimeIdx = tweetTimeIdx.strftime('%m-%d %H:%M')
+            tweetTime = pd.DataFrame(data=None, columns=threeHoursBefore.columns, index=[tweetTimeIdx])
+
+            # tweetTime["Open"][0] = (threeHoursBefore["Open"][-1] + threeHoursAfter["Open"][0])/2
+            # tweetTime["Volume"][0] = (threeHoursBefore["Volume"][-1] + threeHoursAfter["Volume"][0])/2
+
+            sixHoursSpan = pd.concat([threeHoursBefore, tweetTime, threeHoursAfter])
+
+            minIn6Hours = sixHoursSpan["Open"].min()
+            maxIn6Hours = sixHoursSpan["Open"].max()
+
+            # --------first option--------
+            fig = plt.figure(figsize=(16, 9))
+            ax = fig.add_subplot(111)
+            ax.set_title("EMIOTS")
+            ax.set_xlabel("Time")
+            ax.set_ylabel("Price")
+            # plt.xticks(rotation=45)
+            ax.tick_params(axis='x', length=2, pad=2, labelsize=7, labelrotation=85, labelleft=True)
+            # ax.set_view_interval()
+
+            ax.plot(sixHoursSpan.index, sixHoursSpan["Open"], 'r-', label="Stock price", lw=4)
+            ax.plot([sixHoursSpan.index[sixHoursSpan.index.get_loc(key=tweetTimeIdx)],
+                    sixHoursSpan.index[sixHoursSpan.index.get_loc(key=tweetTimeIdx)]], [minIn6Hours, maxIn6Hours],
+                    'k--', lw=3)
+
+            ax2 = ax.twinx()
+            ax2.set_ylabel("Volume")
+            ax2.plot(sixHoursSpan.index, sixHoursSpan["Volume"], 'b-', label="Volume", lw=4)
+
+            mpld3.fig_to_html(fig)
+            plt.show()
+            fig.savefig('my_plot.png')
+            mpld3.save_html(fig, "figure.html")
+
+            # --------second option--------
+            # plt.figure(figsize=(8, 5))
+            # sixHoursSpan["Open"].plot(legend=True, grid=True, title="EMIOTS", ylabel="Stock price", xlabel="Time")
+            # plt.xticks()
+            # sixHoursSpan["Volume"].plot(secondary_y=True, legend=True, grid=True)
+            # plt.show()
+            # plt.xticks(range(0, len(sixHoursSpan.index)), sixHoursSpan.index)
 
             print(idx)
             print(openingValue)
@@ -135,9 +179,10 @@ class StockData:
             print("")
             print(swingInMinus)
             print("")
+            pd.set_option('display.max_rows', None)
             print(sixHoursSpan)
             print("")
-            plt.show()
+            # plt.show()
 
 
 if __name__ == '__main__':
