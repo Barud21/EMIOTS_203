8e271809072da48fbee2b9df72bab71d217ab541
RandomTesterr <34625263+RandomTesterr@users.noreply.github.com>
2020-12-10 20:59:20 +0100

    Emiots django app - url listing tweets with charts

--- EMIOTS203/views.py
+++ EMIOTS203/views.py
@@ -1,3 +1,11 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+
+from .models import Tweet
+
 
 # Create your views here.
+def list_view(request):
+    qs = Tweet.objects.filter(stockchart__isnull=False)
+    template_name = 'EMIOTS203/list.html'
+    context = {"tweet_list": qs}
+    return render(request, template_name, context)
