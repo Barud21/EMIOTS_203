ff677b08a8089e2b045c77fbf9cddbfdfc0006c2
RandomTesterr <34625263+RandomTesterr@users.noreply.github.com>
2021-01-18 19:34:38 +0100

    Filtering form improvements

--- EMIOTS203/forms.py
+++ EMIOTS203/forms.py
@@ -5,12 +5,26 @@ from .models import StockChart
 from .widgets import FengyuanChenDatePickerInput
 
 
-class SwingChoiceField(forms.Form):
+class FilteringForm(forms.Form):
 
-    maxValue = StockChart.objects.aggregate(Max('maxSwing'))['maxSwing__max']
-    swingValues = forms.ChoiceField(choices=[(x, x) for x in range(1, int(maxValue)+1)])
+    maxOfMaxSwing = StockChart.objects.aggregate(Max('maxSwing'))['maxSwing__max']
+    swingChoicesList = [(x, x) for x in range(1, int(maxOfMaxSwing)+1)]
+    swingChoicesList.insert(0, ('', '----'))
+    swingValues = forms.ChoiceField(choices=swingChoicesList,
+                                    label='Stock change greater than X percent:',
+                                    required=False)
 
-    date = forms.DateTimeField(
-        input_formats=['%d/%m/%Y %H:%M'],
-        widget=FengyuanChenDatePickerInput()
+    startDate = forms.DateTimeField(
+        input_formats=['%d/%m/%Y'],
+        widget=FengyuanChenDatePickerInput(),
+        label='From date:',
+        required=False
+    )
+
+    # TODO: Create a custom date picker field to avoid copy pasting
+    endDate = forms.DateTimeField(
+        input_formats=['%d/%m/%Y'],
+        widget=FengyuanChenDatePickerInput(),
+        label='To date:',
+        required=False
     )
--- EMIOTS203/views.py
+++ EMIOTS203/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+import datetime
 
 from .models import Tweet
-from .forms import SwingChoiceField
+from .forms import FilteringForm
 
 # Create your views here.
 
@@ -11,12 +12,28 @@ def tweetsChartsView(request):
     qs = Tweet.objects.filter(stockchart__isnull=False)
     template_name = 'EMIOTS203/list.html'
 
-    form = SwingChoiceField(request.POST or None)
+    form = FilteringForm(request.POST or None)
 
     if request.method == 'POST':
         if(form.is_valid()):
-            selectedSwing = form['swingValues'].value()
-            qs = qs.filter(stockchart__maxSwing__gte=selectedSwing)
+            if form['swingValues'].value() != '':
+                selectedSwing = form['swingValues'].value()
+                qs = qs.filter(stockchart__maxSwing__gte=selectedSwing)
+
+            if form['startDate'].value() != '':
+                startDate = form['startDate'].value()
+                dateTimeStartDate = datetime.datetime.strptime(startDate, '%d/%m/%Y')
+                dateTimeStartDate = dateTimeStartDate.replace(hour=0, minute=0, second=0, tzinfo=datetime.timezone.utc)
+
+                qs = qs.filter(date__gte=dateTimeStartDate)
+
+            # TODO: Think of clever way to handle these 2 datetime values processing
+            if form['endDate'].value() != '':
+                endDate = form['endDate'].value()
+                dateTimeEndDate = datetime.datetime.strptime(endDate, '%d/%m/%Y')
+                dateTimeEndDate = dateTimeEndDate.replace(hour=23, minute=59, second=59, tzinfo=datetime.timezone.utc)
+
+                qs = qs.filter(date__lte=dateTimeEndDate)
 
     paginator = Paginator(qs, 5)
     page_number = request.GET.get('page', 1)
